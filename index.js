c537e5d4eddb9a2c9735ecd4eb057dbe2acd3ffa
Miracle <anajemiracle@gmail.com>
2025-03-08 08:48:33 +0100

    add: 3 websites

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const left = document.querySelector('.left')
 const right = document.querySelector('.right')
+const center = document.querySelector('.center')
 const container = document.querySelector('.container')
 
 left.addEventListener('mouseenter', () => {
@@ -8,6 +9,14 @@ left.addEventListener('mouseenter', () => {
 
 left.addEventListener('mouseleave', () => {
     container.classList.remove('hover-left');
+})
+
+center.addEventListener('mouseenter', () => {
+    container.classList.add('hover-center');
+})
+
+center.addEventListener('mouseleave', () => {
+    container.classList.remove('hover-center');
 })
 
 right.addEventListener('mouseenter', () => {
